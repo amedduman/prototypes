592a982994a791cbc15d05b2f7724594d589c448
amedduman <amedduman@protonmail.com>
2024-06-02 01:23:15 +0300

    fix 2 warnings

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -26,7 +26,7 @@ int main()
 
     Vector2 center = {200, 200};
     float rad = (2 * PI) / (float)MAX_POINTS;
-    Vector2 positions[10] = {0, 0};
+    Vector2 positions[MAX_POINTS] = {0, 0};
     for (int i = 0; i < MAX_POINTS; i++)
     {
         Vector2 p2 = {0, 0};
@@ -70,7 +70,7 @@ int main()
                 DrawCircleV(positions[i], 5, WHITE);
             }
 
-            DrawLineStrip(&positions, MAX_POINTS, SKYBLUE);
+            DrawLineStrip(&positions[0], MAX_POINTS, SKYBLUE);
             DrawLineV(positions[0], positions[MAX_POINTS - 1], SKYBLUE);
         }
     }
