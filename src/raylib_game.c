4a6bf45131ccb8d5d5c6a6e713b936bb0b25075b
amedduman <amedduman@protonmail.com>
2024-06-01 20:30:55 +0300

    draw a star

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,6 +1,9 @@
-#include <raylib.h>
-#include "dino.h"
 #include "defer.h"
+#include "raylib.h"
+#include "rlgl.h" // Required for: Vertex definition
+#include "raymath.h"
+
+#define MAX_POINTS 12
 
 const int screenWidht = 400;
 const int screenHeight = 400;
@@ -8,28 +11,42 @@ const int screenHeight = 400;
 int main()
 {
     InitWindow(screenWidht, screenHeight, "Tetris???");
-    SetTargetFPS(60);
+    SetTargetFPS(4);
 
-    Dino dino;
-    InitDino(&dino);
+    Vector2 center = {200, 200};
+    float rad = (2 * PI) / (float)MAX_POINTS;
+    Vector2 positions[10] = {0, 0};
+    for (int i = 0; i < MAX_POINTS; i++)
+    {
+        Vector2 p2 = {0, 0};
 
-    Rectangle r = {300,300, 100,100};
+        p2 = (Vector2){cosf(i * rad), sinf(i * rad)};
+
+        if (i % 2 == 0)
+            p2 = Vector2Scale(p2, 100);
+        else
+            p2 = Vector2Scale(p2, 50);
+        p2 = Vector2Add(center, p2);
+
+        positions[i] = p2;
+    }
 
     while (!WindowShouldClose())
     {
-        Update(&dino);
-
         defer(BeginDrawing(), EndDrawing())
         {
             ClearBackground(DARKGRAY);
 
-            DrawRectangleLinesEx(r, 5, MAROON);
-            Draw(&dino);
-            bool isColliding = CheckCollisionRecs(dino.rect, r);
-            DrawRect(&dino, isColliding);
-        }   
+            DrawCircleV(center, 5, MAROON);
+            for (int i = 0; i < MAX_POINTS; i++)
+            {
+                DrawCircleV(positions[i], 5, WHITE);
+            }
+
+            DrawLineStrip(&positions, MAX_POINTS, SKYBLUE);
+            DrawLineV(positions[0], positions[MAX_POINTS-1], SKYBLUE);
+        }
     }
-    
-    FreeDinoRes(&dino);
+
     CloseWindow();
 }
