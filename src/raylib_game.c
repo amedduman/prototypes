6ae5bc79f9a3e78987f4576ec82ec03add57cb99
amedduman <amedduman@protonmail.com>
2024-06-03 20:33:56 +0300

    codin math E3 circle moves up down based sin value example

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,6 +1,6 @@
 #include "defer.h"
 #include "raylib.h"
-#include "3dMath.h"
+#include "raymath.h"
 #include <stdio.h>
 
 const int screenWidht = 400;
@@ -12,131 +12,26 @@ int main()
     {
         SetTargetFPS(24);
 
-        MyMesh mesh_cube;
-        mesh_cube.tris = vector_create();
-
-        Tri south1 = {{ {0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 1.0, 0.0} }};
-        Tri south2 = {{ {0.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {1.0, 0.0, 0.0} }};
-        vector_add(&mesh_cube.tris, south1);
-        vector_add(&mesh_cube.tris, south2);
-
-        Tri east1 = {{ {1.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {1.0, 1.0, 1.0} }};
-        Tri east2 = {{ {1.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 0.0, 1.0} }};
-        vector_add(&mesh_cube.tris, east1);
-        vector_add(&mesh_cube.tris, east2);
-
-        Tri north1 = {{ {1.0, 0.0, 1.0}, {1.0, 1.0, 1.0}, {0.0, 1.0, 1.0} }};
-        Tri north2 = {{ {1.0, 0.0, 1.0}, {0.0, 1.0, 1.0}, {0.0, 0.0, 1.0} }};
-        vector_add(&mesh_cube.tris, north1);
-        vector_add(&mesh_cube.tris, north2);
-
-        Tri west1 = {{ {0.0, 0.0, 1.0}, {0.0, 1.0, 1.0}, {0.0, 1.0, 0.0} }};
-        Tri west2 = {{ {0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 0.0} }};
-        vector_add(&mesh_cube.tris, west1);
-        vector_add(&mesh_cube.tris, west2);
-
-        Tri top1 = {{ {0.0, 1.0, 0.0}, {0.0, 1.0, 1.0}, {1.0, 1.0, 1.0} }};
-        Tri top2 = {{ {0.0, 1.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 0.0} }};
-        vector_add(&mesh_cube.tris, top1);
-        vector_add(&mesh_cube.tris, top2);
-
-        Tri bottom1 = {{ {1.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0} }};
-        Tri bottom2 = {{ {1.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0} }};
-        vector_add(&mesh_cube.tris, bottom1);
-        vector_add(&mesh_cube.tris, bottom2);
-
-        bool did = false;
+        Vector2 pos = {screenWidht * 0.5, 0};
+        float radius = 30;
+        float angle = 0;
+        
 
         while (!WindowShouldClose())
         {
+            // * (sinf(angle) < 0 ? 1 : -1)
+            //pos.y = sinf(angle) * screenHeight;
+            
+            pos.y = Lerp(screenHeight * 0.5, radius, sinf(angle));
+            angle += PI * 0.01;
             defer(BeginDrawing(), EndDrawing())
             {
                 ClearBackground(DARKGRAY);
-
-                if(did == false)
-                {
-                    for (int i = 0; i < vector_size(mesh_cube.tris); i++)
-                    {
-                        printf("%d \n", i);
-                    }
-
-                    did = true;
-                }
+                DrawCircleV(pos, radius, MAROON);
+                char angleText[50];
+                snprintf(angleText, 50, "Sin: %.2f", sinf(angle));
+                DrawText(angleText, 100,100, 10, WHITE);
             }
         }
-
-        vector_free(mesh_cube.tris);
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* 
-#include "defer.h"
-#include "raylib.h"
-#include "3dMath.h"
-
-const int screenWidht = 400;
-const int screenHeight = 400;
-
-int main()
-{
-    defer(InitWindow(screenWidht, screenHeight, "Game"), CloseWindow())
-    {
-        SetTargetFPS(24);
-
-        MyMesh mesh_cube;
-        mesh_cube.tris = vector_create();
-
-        Tri south1 = {{0.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 1.0, 0.0}};
-        Tri south2 = {{0.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {1.0, 0.0, 0.0}};
-        vector_add(&mesh_cube.tris, south1);
-        vector_add(&mesh_cube.tris, south2);
-
-        Tri east1 = {{1.0, 0.0, 0.0}, {1.0, 1.0, 0.0}, {1.0, 1.0, 1.0}};
-        Tri east2 = {{1.0, 0.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 0.0, 1.0}};
-        vector_add(&mesh_cube.tris, east1);
-        vector_add(&mesh_cube.tris, east1);
-
-        Tri north1 = {{1.0, 0.0, 1.0}, {1.0, 1.0, 1.0}, {0.0, 1.0, 1.0}};
-        Tri north2 = {{1.0, 0.0, 1.0}, {0.0, 1.0, 1.0}, {0.0, 0.0, 1.0}};
-        vector_add(&mesh_cube.tris, north1);
-        vector_add(&mesh_cube.tris, north2);
-
-        Tri west1 = {{0.0, 0.0, 1.0}, {0.0, 1.0, 1.0}, {0.0, 1.0, 0.0}};
-        Tri west2 = {{0.0, 0.0, 1.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 0.0}};
-        vector_add(&mesh_cube.tris, west1);
-        vector_add(&mesh_cube.tris, west2);
-
-        Tri top1 = {{0.0, 1.0, 0.0}, {0.0, 1.0, 1.0}, {1.0, 1.0, 1.0}};
-        Tri top2 = {{0.0, 1.0, 0.0}, {1.0, 1.0, 1.0}, {1.0, 1.0, 0.0}};
-        vector_add(&mesh_cube.tris, top1);
-        vector_add(&mesh_cube.tris, top2);
-
-        Tri bottom1 = {{1.0, 0.0, 1.0}, {0.0, 0.0, 1.0}, {0.0, 0.0, 0.0}};
-        Tri bottom2 = {{1.0, 0.0, 1.0}, {0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}};
-        vector_add(&mesh_cube.tris, bottom1);
-        vector_add(&mesh_cube.tris, bottom2);
-
-
-        while (!WindowShouldClose())
-        {
-            defer(BeginDrawing(), EndDrawing())
-            {
-                ClearBackground(DARKGRAY);
-            }
-        }
-
-        vector_free(mesh_cube.tris);
-    }
-} */
