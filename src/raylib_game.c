e62ad8abe4927127928a71ca591291de6a7fc8f4
amedduman <amedduman@protonmail.com>
2024-06-03 22:35:22 +0300

    draw circle while leaving trail behind

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -2,6 +2,7 @@
 #include "raylib.h"
 #include "raymath.h"
 #include <stdio.h>
+#include "vec.h"
 
 const int screenWidht = 400;
 const int screenHeight = 400;
@@ -16,21 +17,40 @@ int main()
         float radius = 30;
         float angle = 0;
 
+        Vector2* circles = vector_create();
+
         while (!WindowShouldClose())
         {
+            float cosVal = (cosf(angle) + 1) * 0.5;
             float sinVal = (sinf(angle) + 1) * 0.5;
+
+            pos.x = Lerp(screenWidht - radius, radius, cosVal);
             pos.y = Lerp(screenHeight - radius, radius, sinVal);
+
+            vector_add(&circles, pos);
 
             angle += PI * 0.01;
 
-            char angleText[50];
-            snprintf(angleText, 50, "Sin: %.2f", sinVal);
+            char sinValText[50];
+            snprintf(sinValText, 50, "Sin: %.2f", sinVal);
+
+            char cosValText[50];
+            snprintf(cosValText, 50, "Cos: %.2f", cosVal);
+
+            
 
             defer(BeginDrawing(), EndDrawing())
             {
                 ClearBackground(DARKGRAY);
-                DrawCircleV(pos, radius, MAROON);
-                DrawText(angleText, 100, 100, 10, WHITE);
+                
+                for (int i = 0; i < vector_size(circles); i++)
+                {
+                    DrawCircleV(circles[i], radius, MAROON);
+                }
+                
+
+                DrawText(cosValText, 100, 110, 10, WHITE);
+                DrawText(sinValText, 100, 100, 10, WHITE);
             }
         }
     }
