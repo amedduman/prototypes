75dbcba1ca51c3ac8e262bd96f16d02768b51f67
amedduman <amedduman@protonmail.com>
2024-06-05 03:09:17 +0300

    add a shitty confetti CodingMath Ep:9

--- src/Particle.h
+++ src/Particle.h
@@ -6,20 +6,23 @@ typedef struct Particle
 {
     Vector2 pos;
     Vector2 vel;
+    Vector2 acc;
 }Particle;
 
-void particle_create(Particle* particle, Vector2 pos, Vector2 vel)
+void particle_create(Particle* particle, Vector2 pos, Vector2 vel, Vector2 acc)
 {
     particle->pos = pos;
     particle->vel = vel;
+    particle->acc = acc;
 }
 
 void particle_update(Particle* particle)
 {
+    particle->vel = Vector2Add(particle->vel, particle->acc);
     particle->pos = Vector2Add(particle->pos, particle->vel);
 }
 
 void particle_draw(Particle* particle)
 {
-    DrawCircleV(particle->pos, 10, WHITE);
+    DrawCircleV(particle->pos, 5, WHITE);
 }
--- src/raylib_game.c
+++ src/raylib_game.c
@@ -8,35 +8,48 @@
 const int screenWidht = 400;
 const int screenHeight = 400;
 
+float GetRandomFloat(float min, float max)
+{
+    // Generate a random integer using GetRandomValue
+    int randomInt = GetRandomValue(0, 10000);
+
+    // Scale the random integer to the desired float range
+    float randomFloat = ((float)randomInt / 10000.0f) * (max - min) + min;
+
+    return randomFloat;
+}
+
 int main()
 {
     defer(InitWindow(screenWidht, screenHeight, "Math"), CloseWindow())
     {
         SetTargetFPS(60);
 
+        Vector2 gravity = {0, 0.2};
         Particle *particles = vector_create();
 
         for (int i = 0; i < 100; i++)
         {
             Particle p;
-            particle_create(&p, (Vector2){GetRandomValue(0, 400),GetRandomValue(0, 400)}, (Vector2){GetRandomValue(-3, 3), GetRandomValue(-3,3)});
+            particle_create(&p, (Vector2){200, 200}, (Vector2){GetRandomFloat(-5, 5), GetRandomFloat(-5, 5)}, gravity);
             vector_add(&particles, p);
         }
 
         while (!WindowShouldClose())
         {
-            for (int i = 0; i < vector_size(particles); i++)
+            if (IsKeyDown(KEY_A))
             {
-                particle_update(&particles[i]);
+                for (int i = 0; i < vector_size(particles); i++)
+                {
+                    particle_update(&particles[i]);
+                }
             }
-
             defer(BeginDrawing(), EndDrawing())
             {
                 ClearBackground(GOLD);
 
                 for (int i = 0; i < vector_size(particles); i++)
                 {
-
                     particle_draw(&particles[i]);
                 }
             }
