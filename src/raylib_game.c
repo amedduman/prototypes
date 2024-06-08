f00a7f2fc1fa257454b1588ccb40060cc442ea49
amedduman <amedduman@protonmail.com>
2024-06-08 21:54:04 +0300

    clean up

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -2,18 +2,6 @@
 #include "utils.h"
 #include <raylib.h>
 #include <raymath.h>
-#include <stdio.h>
-
-void DragCircle(Vector2 *circleCenter, float radius)
-{
-    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
-    {
-        if (ut_IsPointCircleIntersect(GetMousePosition(), *circleCenter, radius + radius))
-        {
-            *circleCenter = GetMousePosition();
-        }
-    }
-}
 
 int main()
 {
@@ -22,54 +10,12 @@ int main()
 
     defer(InitWindow(screenWidth, screenHeight, "Math"), CloseWindow())
     {
-        SetTargetFPS(60);
-
-        Vector2 p0 = {100, 100};
-        Vector2 p1 = {200, 100};
-        Vector2 p2 = {300, 200};
-        Vector2 p3 = {300, 300};
-        Vector2 cp = {0,0};
-        float radius = 5;
-
-        cp.x = (p0.x + 2 * p1.x + p2.x) / 4;
-        cp.y = (p0.y + 2 * p1.y + p2.y) / 4;
-
         while (!WindowShouldClose())
         {
-            DragCircle(&p0, radius);
-            DragCircle(&cp, radius);
-            DragCircle(&p2, radius);
-
-            //DragCircle(&p2, radius);
-            //DragCircle(&p3, radius);
-            
-            /*
-            cp.x = p1.x * 2 - (p0.x + p2.x) / 2;
-	        cp.y = p1.y * 2 - (p0.y + p2.y) / 2;
-            cp.x = (p0.x + 2 * p1.x + p2.x) / 4;
-            cp.y = (p0.y + 2 * p1.y + p2.y) / 4;
-            */
-
-            p1.x = (4 * cp.x - p0.x - p2.x) / 2;
-            p1.y = (4 * cp.y - p0.y - p2.y) / 2;
-
-
             defer(BeginDrawing(), EndDrawing())
             {
                 ClearBackground(GOLD);
 
-                for (float t = 0; t < 1; t += 0.01f)
-                {
-                    //Vector2 circleCenter = ut_cubicBezier(p0, p1, p2, p3, t);
-                    Vector2 circleCenter = ut_QuadraticBezier(p0, p1, p2, t);
-                    DrawCircleLinesV(circleCenter, 3, RED);
-                }
-
-                DrawCircleV(p0, radius, WHITE);
-                DrawCircleV(p1, radius, GRAY);
-                DrawCircleV(p2, radius, WHITE);
-                //DrawCircleV(p3, radius, WHITE);
-                DrawCircleV(cp, radius, RED);
             }
         }
     }
--- src/utils.h
+++ src/utils.h
@@ -17,34 +17,58 @@ bool ut_IsPointCircleIntersect(Vector2 point, Vector2 center, float radius)
     return Vector2DistanceSqr(center, point) < radius * radius;
 }
 
-Vector2 ut_QuadraticBezier(Vector2 p0, Vector2 p1, Vector2 p2, float t)
+Vector2 ut_quadraticBezier(Vector2 p0, Vector2 p1, Vector2 p2, float t)
 {
-    Vector2 pFinal = {0,0};
-    
-    pFinal.x = powf(1 - t, 2) * p0.x + 
-                (1 - t) * 2 * t * p1.x + 
-                t * t * p2.x;
+    Vector2 pFinal = {0, 0};
 
-    pFinal.y = powf(1 - t, 2) * p0.y + 
-                (1 - t) * 2 * t * p1.y + 
-                t * t * p2.y;
+    pFinal.x = powf(1 - t, 2) * p0.x +
+               (1 - t) * 2 * t * p1.x +
+               t * t * p2.x;
 
-	return pFinal;
+    pFinal.y = powf(1 - t, 2) * p0.y +
+               (1 - t) * 2 * t * p1.y +
+               t * t * p2.y;
+
+    return pFinal;
 }
 
 Vector2 ut_cubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t)
 {
-    Vector2 pFinal = {0,0};
+    Vector2 pFinal = {0, 0};
 
-    pFinal.x = powf(1 - t, 3) * p0.x + 
-                powf(1 - t, 2) * 3 * t * p1.x + 
-                (1 - t) * 3 * t * t * p2.x + 
-                t * t * t * p3.x;
+    pFinal.x = powf(1 - t, 3) * p0.x +
+               powf(1 - t, 2) * 3 * t * p1.x +
+               (1 - t) * 3 * t * t * p2.x +
+               t * t * t * p3.x;
 
-    pFinal.y = powf(1 - t, 3) * p0.y + 
-                powf(1 - t, 2) * 3 * t * p1.y + 
-                (1 - t) * 3 * t * t * p2.y + 
-                t * t * t * p3.y;
+    pFinal.y = powf(1 - t, 3) * p0.y +
+               powf(1 - t, 2) * 3 * t * p1.y +
+               (1 - t) * 3 * t * t * p2.y +
+               t * t * t * p3.y;
 
     return pFinal;
+}
+
+
+Vector2 ut_drawMultiCurve(Vector2* points, int length)
+{
+    Vector2 p0;
+    Vector2 p1;
+    float midx;
+    float midy;
+
+    for(int i = 0; i < length - 2; i += 1)
+    {
+        p0 = points[i];
+        p1 = points[i + 1];
+        midx = (p0.x + p1.x) / 2.0f;
+        midy = (p0.y + p1.y) / 2.0f;
+        DrawSplineSegmentBezierQuadratic(p0, (Vector2){midx, midy}, p1, 3, BLACK);
+    }
+
+    p0 = points[length - 2];
+    p1 = points[length - 1];
+    midx = (p0.x + p1.x) / 2.0f;
+    midy = (p0.y + p1.y) / 2.0f;
+    DrawSplineSegmentBezierQuadratic(p0, (Vector2){midx, midy}, p1, 3, BLACK);
 }
