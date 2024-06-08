bb85a1ccc03ae7d53c4d55f658ebf9017e2852d0
amedduman <amedduman@protonmail.com>
2024-06-08 16:34:29 +0300

    control curve with control point

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -31,19 +31,27 @@ int main()
         Vector2 cp = {0,0};
         float radius = 5;
 
+        cp.x = (p0.x + 2 * p1.x + p2.x) / 4;
+        cp.y = (p0.y + 2 * p1.y + p2.y) / 4;
+
         while (!WindowShouldClose())
         {
             DragCircle(&p0, radius);
-            DragCircle(&p1, radius);
+            DragCircle(&cp, radius);
             DragCircle(&p2, radius);
-            DragCircle(&p3, radius);
+
+            //DragCircle(&p2, radius);
+            //DragCircle(&p3, radius);
             
             /*
             cp.x = p1.x * 2 - (p0.x + p2.x) / 2;
 	        cp.y = p1.y * 2 - (p0.y + p2.y) / 2;
-            */
             cp.x = (p0.x + 2 * p1.x + p2.x) / 4;
             cp.y = (p0.y + 2 * p1.y + p2.y) / 4;
+            */
+
+            p1.x = (4 * cp.x - p0.x - p2.x) / 2;
+            p1.y = (4 * cp.y - p0.y - p2.y) / 2;
 
 
             defer(BeginDrawing(), EndDrawing())
@@ -60,7 +68,7 @@ int main()
                 DrawCircleV(p0, radius, WHITE);
                 DrawCircleV(p1, radius, GRAY);
                 DrawCircleV(p2, radius, WHITE);
-                DrawCircleV(p3, radius, WHITE);
+                //DrawCircleV(p3, radius, WHITE);
                 DrawCircleV(cp, radius, RED);
             }
         }
