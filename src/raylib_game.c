a3b5992ef05707a7b679381b7da24fa4887ffa6e
amedduman <amedduman@protonmail.com>
2024-06-09 17:47:47 +0300

    project 3d points in 2d screen

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -8,59 +8,51 @@
 typedef struct {
   Vector3 pos;
   Vector3 size;
-} postcard;
+  Vector2 screen_pos;
+} point;
+
+void project(point *points, int arrLen, float fl, int screenWidth,
+             int screenHeight) {
+  for (int i = 0; i < arrLen; i++) {
+    float perspective = fl / (fl + points[i].pos.z);
+
+    points[i].screen_pos.x =
+        points[i].pos.x * perspective + (float)screenWidth / 2;
+    points[i].screen_pos.y =
+        points[i].pos.y * perspective + (float)screenHeight / 2;
+  }
+}
 
 int main() {
-  const int screenWidth = 400;
-  const int screenHeight = 400;
-  const int arrayLength = 30;
+  const int screenWidth = 800;
+  const int screenHeight = 800;
+
+  float fl = 300;
+  int arrLen = 8;
+  point points[arrLen];
+  float radius = 5;
+
+  points[0].pos = (Vector3){-500, -500, 1000};
+  points[1].pos = (Vector3){500, -500, 1000};
+  points[2].pos = (Vector3){500, -500, 500};
+  points[3].pos = (Vector3){-500, -500, 500};
+  points[4].pos = (Vector3){-500, 500, 1000};
+  points[5].pos = (Vector3){500, 500, 1000};
+  points[6].pos = (Vector3){500, 500, 500};
+  points[7].pos = (Vector3){-500, 500, 500};
+
+  project(points, arrLen, fl, screenWidth, screenHeight);
 
   defer(InitWindow(screenWidth, screenHeight, "Math"), CloseWindow()) {
     SetTargetFPS(60);
 
-    postcard *postcards[arrayLength];
-    float radius = 300;
-    float centerZ = 300;
-    float fl = 300.0;
-    float baseAngle = 0;
-
     while (!WindowShouldClose()) {
 
-      baseAngle += 0.5f * GetFrameTime();
+      defer(BeginDrawing(), EndDrawing()) { ClearBackground(GOLD); }
 
-      for (int i = 0; i < arrayLength; i++) {
-        // Allocate memory for each postcard
-        postcards[i] = (postcard *)malloc(sizeof(postcard));
-
-        float angle = PI * 2 / arrayLength * i + baseAngle;
-        float pos_x = cosf(angle) * radius;
-        float pos_y = 0;
-        float pos_z = sinf(angle) * radius + centerZ;
-
-        int cardSize = 30;
-        postcards[i]->pos = (Vector3){pos_x, pos_y, pos_z};
-        postcards[i]->size = (Vector3){cardSize, cardSize, cardSize};
+      for (int i = 0; i < arrLen; i++) {
+        DrawCircleV(points[i].screen_pos, radius, BLACK);
       }
-
-      defer(BeginDrawing(), EndDrawing()) {
-        ClearBackground(GOLD);
-
-        for (int i = 0; i < arrayLength; i++) {
-          float perspective = fl / (fl + postcards[i]->pos.z);
-          Vector2 screen_pos = {
-              (postcards[i]->pos.x * perspective) + (float)screenWidth / 2,
-              (postcards[i]->pos.y * perspective) + (float)screenHeight / 2};
-          Vector2 screen_size = {postcards[i]->size.x * perspective,
-                                 postcards[i]->size.y * perspective};
-
-          DrawRectangleV(screen_pos, screen_size, BLACK);
-        }
-      }
-    }
-
-    // Free allocated memory
-    for (int i = 0; i < arrayLength; i++) {
-      free(postcards[i]);
     }
   }
 }
