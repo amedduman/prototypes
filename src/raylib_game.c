ae8adc319ecd17a2fcac101f8d1bcf7b7cf1c3af
amedduman <amedduman@protonmail.com>
2024-06-09 15:18:50 +0300

    draw 3D Carousel

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -1,77 +1,65 @@
 #include "defer.h"
 #include "utils.h"
+#include <math.h>
 #include <raylib.h>
 #include <raymath.h>
 #include <stdlib.h>
 
-typedef struct
-{
-    Vector3 pos;
-    Vector3 size;
+typedef struct {
+  Vector3 pos;
+  Vector3 size;
 } postcard;
 
-int main()
-{
-    const int screenWidth = 400;
-    const int screenHeight = 400;
-    const int arrayLength = 30;
+int main() {
+  const int screenWidth = 400;
+  const int screenHeight = 400;
+  const int arrayLength = 30;
 
-    defer(InitWindow(screenWidth, screenHeight, "Math"), CloseWindow())
-    {
-        postcard *postcards[arrayLength];
-        float card_z = 500;
-        for (int i = 0; i < arrayLength; i++)
-        {
-            // Allocate memory for each postcard
-            postcards[i] = (postcard *)malloc(sizeof(postcard));
+  defer(InitWindow(screenWidth, screenHeight, "Math"), CloseWindow()) {
+    SetTargetFPS(1);
 
-            int rnd_pos_x = GetRandomValue(-screenWidth / 2, screenWidth / 2);
-            int rnd_pos_y = GetRandomValue(-screenHeight / 2, screenHeight / 2);
-            int rnd_size_x = GetRandomValue(20, 100);
-            int rnd_size_y = GetRandomValue(20, 100);
+    postcard *postcards[arrayLength];
+    float radius = 300;
+    float centerZ = 300;
 
-            postcards[i]->pos = (Vector3){rnd_pos_x, rnd_pos_y, card_z};
-            postcards[i]->size = (Vector3){rnd_size_x, rnd_size_y, 50};
-        }
+    for (int i = 0; i < arrayLength; i++) {
+      // Allocate memory for each postcard
+      postcards[i] = (postcard *)malloc(sizeof(postcard));
 
-        float fl = 300.0;
+      float angle = PI * 2 / arrayLength * i;
+      float pos_x = cosf(angle) * radius;
+      float pos_y = 0;
+      float pos_z = sinf(angle) * radius + centerZ;
 
-        while (!WindowShouldClose())
-        {
-            /*
-            screen_pos = (Vector2){obj_pos.x * perspective, obj_pos.y * perspective};
-            screen_size = (Vector2){obj_size.x * perspective, obj_pos.y * perspective};
+      int size_x = 50;
+      int size_y = 50;
 
-            */
-
-            float perspective = fl / (fl + card_z);
-            card_z -= 5 * GetFrameTime() * 10;
-            if (card_z < 0)
-            {
-                card_z = 500;
-            }
-
-            defer(BeginDrawing(), EndDrawing())
-            {
-                ClearBackground(GOLD);
-
-                for (int i = 0; i < arrayLength; i++)
-                {
-                    Vector2 screen_pos = {
-                        (postcards[i]->pos.x * perspective) + screenWidth / 2,
-                        (postcards[i]->pos.y * perspective) + screenHeight / 2
-                        };
-                    Vector2 screen_size = {postcards[i]->size.x * perspective, postcards[i]->size.y * perspective};
-
-                    DrawRectangleV(screen_pos, screen_size, DARKBLUE);
-                }
-            }
-        }
-
-        // Free allocated memory
-        for (int i = 0; i < arrayLength; i++)
-        {
-            free(postcards[i]);
-        }
+      postcards[i]->pos = (Vector3){pos_x, pos_y, pos_z};
+      postcards[i]->size = (Vector3){size_x, size_y, 50};
     }
+
+    float fl = 300.0;
+
+    while (!WindowShouldClose()) {
+      defer(BeginDrawing(), EndDrawing()) {
+        ClearBackground(GOLD);
+
+        for (int i = 0; i < arrayLength; i++) {
+          float perspective = fl / (fl + postcards[i]->pos.z);
+          Vector2 screen_pos = {
+              (postcards[i]->pos.x * perspective) + (float)screenWidth / 2,
+              (postcards[i]->pos.y * perspective) + (float)screenHeight / 2};
+          Vector2 screen_size = {postcards[i]->size.x * perspective,
+                                 postcards[i]->size.y * perspective};
+
+          DrawRectangleV(screen_pos, screen_size, BLACK);
+        }
+      }
+    }
+
+    // Free allocated memory
+    for (int i = 0; i < arrayLength; i++) {
+      free(postcards[i]);
+    }
+  }
 }
--- src/utils.h
+++ src/utils.h
@@ -2,51 +2,44 @@
 #include <raylib.h>
 #include <raymath.h>
 
-float ut_Rndf(float min, float max)
-{
-    return ((float)GetRandomValue(0, 10000) / 10000.0f) * (max - min) + min;
+float ut_Rndf(float min, float max) {
+  return ((float)GetRandomValue(0, 10000) / 10000.0f) * (max - min) + min;
 }
 
-Vector2 ut_Vector2MulVal(Vector2 vec, float val)
-{
-    return (Vector2){vec.x * val, vec.y * val};
+Vector2 ut_Vector2MulVal(Vector2 vec, float val) {
+  return (Vector2){vec.x * val, vec.y * val};
 }
 
-bool ut_IsPointCircleIntersect(Vector2 point, Vector2 center, float radius)
-{
-    return Vector2DistanceSqr(center, point) < radius * radius;
+bool ut_IsPointCircleIntersect(Vector2 point, Vector2 center, float radius) {
+  return Vector2DistanceSqr(center, point) < radius * radius;
 }
 
-Vector2 ut_quadraticBezier(Vector2 p0, Vector2 p1, Vector2 p2, float t)
-{
-    Vector2 pFinal = {0, 0};
+Vector2 ut_quadraticBezier(Vector2 p0, Vector2 p1, Vector2 p2, float t) {
+  Vector2 pFinal = {0, 0};
 
-    pFinal.x = powf(1 - t, 2) * p0.x +
-               (1 - t) * 2 * t * p1.x +
-               t * t * p2.x;
+  pFinal.x = powf(1 - t, 2) * p0.x + (1 - t) * 2 * t * p1.x + t * t * p2.x;
 
-    pFinal.y = powf(1 - t, 2) * p0.y +
-               (1 - t) * 2 * t * p1.y +
-               t * t * p2.y;
+  pFinal.y = powf(1 - t, 2) * p0.y + (1 - t) * 2 * t * p1.y + t * t * p2.y;
 
-    return pFinal;
+  return pFinal;
 }
 
-Vector2 ut_cubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3, float t)
-{
-    Vector2 pFinal = {0, 0};
+Vector2 ut_cubicBezier(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3,
+                       float t) {
+  Vector2 pFinal = {0, 0};
 
-    pFinal.x = powf(1 - t, 3) * p0.x +
-               powf(1 - t, 2) * 3 * t * p1.x +
-               (1 - t) * 3 * t * t * p2.x +
-               t * t * t * p3.x;
+  pFinal.x = powf(1 - t, 3) * p0.x + powf(1 - t, 2) * 3 * t * p1.x +
+             (1 - t) * 3 * t * t * p2.x + t * t * t * p3.x;
 
-    pFinal.y = powf(1 - t, 3) * p0.y +
-               powf(1 - t, 2) * 3 * t * p1.y +
-               (1 - t) * 3 * t * t * p2.y +
-               t * t * t * p3.y;
+  pFinal.y = powf(1 - t, 3) * p0.y + powf(1 - t, 2) * 3 * t * p1.y +
+             (1 - t) * 3 * t * t * p2.y + t * t * t * p3.y;
 
-    return pFinal;
+  return pFinal;
+}
+
+Color GetRandomColor() {
+  return (Color){GetRandomValue(0, 255), GetRandomValue(0, 255),
+                 GetRandomValue(0, 255), GetRandomValue(255, 255)};
 }
 
 /*
@@ -63,7 +56,8 @@ Vector2 ut_drawMultiCurve(Vector2* points, int length)
         p1 = points[i + 1];
         midx = (p0.x + p1.x) / 2.0f;
         midy = (p0.y + p1.y) / 2.0f;
-        DrawSplineSegmentBezierQuadratic(p0, (Vector2){midx, midy}, p1, 3, BLACK);
+        DrawSplineSegmentBezierQuadratic(p0, (Vector2){midx, midy}, p1, 3,
+BLACK);
     }
 
     p0 = points[length - 2];
