e088d73858bc6689813550ff1f3725a02f3ab10e
amedduman <amedduman@protonmail.com>
2024-06-12 13:56:36 +0300

    start verlet integration

--- src/raylib_game.c
+++ src/raylib_game.c
@@ -5,58 +5,38 @@
 #include <raymath.h>
 #include <stdio.h>
 
-typedef struct {
-  bool isIntersecting;
-  Vector2 intersectionPoint;
-} utLineIntersectionResult;
+#define POINTS_NUM 1
 
-bool utisPointOnSegment(Vector2 p, Vector2 p0, Vector2 p1) 
+typedef struct
 {
-  return p.y >= fminf(p0.y, p1.y) && p.y <= fmaxf(p0.y, p1.y) &&
-         p.x >= fminf(p0.x, p1.x) && p.x <= fmaxf(p0.x, p1.x);
+  Vector2 pos;
+  Vector2 oldPos;
+} point;
+
+void point_update(point* point)
+{
+  Vector2 vel = Vector2Subtract(point->pos, point->oldPos);
+  point->oldPos = point->pos;
+  point->pos = Vector2Add(point->pos, vel);
+
+  // check for screen bounds
+  int w = GetScreenWidth();
+  int h = GetScreenHeight();
+  if (point->pos.x > w || point->pos.x < 0)
+  {
+    point->pos.x = point->pos.x > 0 ? w : 0;
+    point->oldPos.x = point->pos.x + vel.x;
+  }
+  if(point->pos.y > h || point->pos.y < 0)
+  {
+    point->pos.y = point->pos.y > 0 ? h : 0;
+    point->oldPos.y = point->pos.y + vel.y;
+  }
 }
 
-utLineIntersectionResult utcheckLineIntersect(Vector2 p0, Vector2 p1, Vector2 p2, Vector2 p3)
+void point_draw(point* point)
 {
-  float A1 = p1.y - p0.y;
-  float B1 = p0.x - p1.x;
-  float C1 = A1 * p0.x + B1 * p0.y;
-
-  float A2 = p3.y - p2.y;
-  float B2 = p2.x - p3.x;
-  float C2 = A2 * p2.x + B2 * p2.y;
-
-  float denominator = A1 * B2 - A2 * B1;
-
-  // check if lines are parallel or colinear
-  if (utfloatComp(denominator, 0))
-  {
-    return (utLineIntersectionResult){false, {0, 0}};
-  }
-
-  float intersectX = (B2 * C1 - B1 * C2) / denominator;
-  float intersectY = (A1 * C2 - A2 * C1) / denominator;
-  Vector2 intersection = {intersectX, intersectY};
-  
-  if (utisPointOnSegment(intersection, p0, p1)
-      &&
-      utisPointOnSegment(intersection, p2, p3))
-  {
-    return (utLineIntersectionResult){true, intersection};
-  }
-
-  return (utLineIntersectionResult){false, {0, 0}};
-}
-
-void DragCircle(Vector2 *circleCenter, float radius)
-{
-    if (IsMouseButtonDown(MOUSE_LEFT_BUTTON))
-    {
-        if (ut_IsPointCircleIntersect(GetMousePosition(), *circleCenter, radius + radius))
-        {
-            *circleCenter = GetMousePosition();
-        }
-    }
+  DrawCircleV(point->pos, 5, WHITE);
 }
 
 int main() 
@@ -66,32 +46,27 @@ int main()
 
   defer(InitWindow(screenWidth, screenHeight, "Math"), CloseWindow()) 
   {
-
     SetTargetFPS(60);
 
-    Vector2 p0 = {100, 100};
-    Vector2 p1 = {100, 350};
-    Vector2 p2 = {0, 300};
-    Vector2 p3 = {350, 300};
+    point points[POINTS_NUM];
+    points[0] = (point){{100,100}, {92,95}};
 
     while (!WindowShouldClose()) 
     {
+
+      for (int i = 0; i < POINTS_NUM; i++)
+      {
+        point_update(&points[i]);
+      }
 
       defer(BeginDrawing(), EndDrawing()) 
       {
         ClearBackground(GOLD);
 
-        DragCircle(&p0, 10);
-        DragCircle(&p1, 10);
-        DragCircle(&p2, 10);
-        DragCircle(&p3, 10);
-
-        utLineIntersectionResult result = utcheckLineIntersect(p0, p1, p2, p3);
-        if (result.isIntersecting) {
-          DrawCircleV(result.intersectionPoint, 15, MAROON);
+        for (int i = 0; i < POINTS_NUM; i++)
+        {
+          point_draw(&points[i]);
         }
-        DrawLineEx(p0, p1, 3, WHITE);
-        DrawLineEx(p2, p3, 3, BLACK);
       }
     }
   }
