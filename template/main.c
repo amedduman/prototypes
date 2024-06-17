f57ad117859573f19630937c708477901e95782d
amedduman <amedduman@protonmail.com>
2024-06-18 01:33:13 +0300

    add template

---
+++ template/main.c
@@ -0,0 +1,25 @@
+#include "include/raylib.h"
+
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+
+    InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        EndDrawing();
+    }
+    CloseWindow();
+    return 0;
+}
+
+/*
+  clear && clang main.c -I include/  -L lib/ -lraylib -framework CoreFoundation -framework IOKit -framework Cocoa && ./a.out
+*/
