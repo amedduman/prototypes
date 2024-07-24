1351f92867815b15fcf86dd11acf87a2669792db
amedduman <amedduman@protonmail.com>
2024-07-24 14:33:36 +0300

    add analytic solution for ray intersection

--- ray-tracing/main.c
+++ ray-tracing/main.c
@@ -37,41 +37,43 @@
 
   typedef struct
   {
-    bool is_intersection_found;
-    Vector3 closest_intersection_point;
-    Vector3 farthest_intersection_point;
+    bool is_intersecting;
+    Vector3 hit_point;
   } ray_sphere_intersection_info;
 
   // https://www.youtube.com/watch?v=OCZTVpfMSys
   ray_sphere_intersection_info ray_sphere_intersection(Vector3 so, float sr, Vector3 ro, Vector3 rd)
   {
-    assert(Vector3Length(rd) < 1 + EPSILON);
+    Vector3 sphere_to_ray_origin = Vector3Subtract(ro, so);
 
-    ray_sphere_intersection_info result = {false, {0,0,0}, {0,0,0}};
+    float a = Vector3DotProduct(rd, rd);
+    float b = 2 * Vector3DotProduct(sphere_to_ray_origin, rd);
+    float c = Vector3DotProduct(sphere_to_ray_origin, sphere_to_ray_origin) - (sr * sr);
 
-    // vector between ray origin and sphere origin 
-    Vector3 l = Vector3Subtract(so, ro);
-    // dot product of l and rd
-    float t = Vector3DotProduct(l, rd);
+    float discriminant = (b * b) - (4 * a * c); 
 
-    // nearest distance between sphere origin and ray 
-    float d = sqrtf(Vector3LengthSqr(l) - t*t);
-
-    if (d < 0 || d > sr)
+    if (discriminant < 0)
     {
-      return (ray_sphere_intersection_info) {false, {-1,-1,-1}, {-1,-1,-1}};
+      return (ray_sphere_intersection_info) {false, (Vector3){-1,-1,-1}};
     }
 
-    if (t < 0)
+    float t1 = (-b + sqrt(discriminant)) / (2*a);
+    float t2 = (-b - sqrt(discriminant)) / (2*a);
+
+    if (t1 > 1)
     {
-      return (ray_sphere_intersection_info) {false, {-1,-1,-1}, {-1,-1,-1}};
+      Vector3 p = Vector3Add(ro, Vector3Scale(rd, t1));
+
+      return (ray_sphere_intersection_info) {true, p};
+    }
+    if (t2 > 1)
+    {
+      Vector3 p = Vector3Add(ro, Vector3Scale(rd, t2));
+
+      return (ray_sphere_intersection_info) {true, p};
     }
 
-    float x = sqrtf(sr*sr - d*d);
-    Vector3 t0 = Vector3Add(ro, Vector3Scale(rd, t - x));
-    Vector3 t1 = Vector3Add(ro, Vector3Scale(rd, t + x));
-    
-    return (ray_sphere_intersection_info) {true, t0, t1};
+    return (ray_sphere_intersection_info) {false, (Vector3){-1,-1,-1}};
   }
 // #endregion
 
@@ -82,7 +84,7 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Game");
     
-    Vector2 startPos = {200,200};
+    Vector2 startPos = {220,220};
     Vector2 endPos = {0,0};
 
     SetTargetFPS(60);
@@ -94,24 +96,27 @@ int main(void)
 
       ClearBackground(RAYWHITE);
 
-      DrawLineV(startPos, endPos, BLACK);
+      Vector3 dir = Vector3Normalize(Vector3Subtract((Vector3){endPos.x, endPos.y, 0}, (Vector3){startPos.x, startPos.y, 0}));
+      dir = Vector3Subtract((Vector3){endPos.x, endPos.y, 0}, (Vector3){startPos.x, startPos.y, 0});
 
       ray_sphere_intersection_info intersection_info = ray_sphere_intersection(
       (Vector3){200,200,0},
       50, 
       (Vector3){startPos.x,startPos.y,0},
-      Vector3Normalize(Vector3Subtract((Vector3){endPos.x, endPos.y, 0}, (Vector3){startPos.x, startPos.y, 0}))
+      dir 
       );
 
-      if (intersection_info.is_intersection_found)
+      if (intersection_info.is_intersecting)
       {
         DrawCircleV((Vector2){200,200}, 50, GREEN);
-        DrawCircleV((Vector2){intersection_info.closest_intersection_point.x, intersection_info.closest_intersection_point.y}, 10, BLACK);
+        DrawCircleV((Vector2){intersection_info.hit_point.x, intersection_info.hit_point.y}, 10, BLACK);
       }
       else
       {
         DrawCircleV((Vector2){200,200}, 50, RED);
       }
+
+      DrawLineEx(startPos, endPos, 3, BLACK);
       
       EndDrawing();
     }
