f72a9559c6384ab9abdf42b3f0833d8cae3f1463
amedduman <amedduman@protonmail.com>
2024-07-24 17:33:27 +0300

    rendering first spheres. no light

--- ray-tracing/main.c
+++ ray-tracing/main.c
@@ -4,28 +4,34 @@
 #include <math.h>
 #include <stdio.h>
 
+// #region Sphere
+typedef struct
+{
+  Vector3 center;
+  float radius;
+  Color color;
+} Sphere;
+// #endregion
+
 // #region Canvas
 
   void canvas_put_pixel(int Cx, int Cy, Color color)
   {
-    float Cw = GetScreenWidth(); // the canvas width
-    float Ch = GetScreenHeight(); // the canvas height
-
-    int Sx = Cw / 2 + Cx;
-    int Sy = Ch / 2 - Cy;
+    int Sx = GetScreenWidth() / 2 + Cx;
+    int Sy = GetScreenHeight() / 2 - Cy;
 
     DrawPixel(Sx, Sy, color);
   }
 
  
-  Vector3 canvas_to_viewport(int Cx, int Cy, int Cw, int Ch)
+  Vector3 canvas_to_viewport(int Cx, int Cy)
   {
     float Vw = 1.0f; // the viewport height
     float Vh = 1.0f; // the viewport width
     float d = 1.0f; // the distance from the camera to the canvas
 
-    float Vx = Cx * Vw / Cw;
-    float Vy = Cy * Vh / Ch;
+    float Vx = Cx * (Vw / GetScreenWidth());
+    float Vy = Cy * (Vh / GetScreenHeight());
     float Vz = d;
 
     return (Vector3){ Vx, Vy, Vz };
@@ -38,40 +44,82 @@
   typedef struct
   {
     bool is_intersecting;
-    Vector3 hit_point;
+    Vector2 intersections;
   } ray_sphere_intersection_info;
 
   // https://www.youtube.com/watch?v=OCZTVpfMSys
   ray_sphere_intersection_info ray_sphere_intersection(Vector3 so, float sr, Vector3 ro, Vector3 rd)
   {
-    Vector3 sphere_to_ray_origin = Vector3Subtract(ro, so);
+    Vector3 CO = Vector3Subtract(ro, so);
 
     float a = Vector3DotProduct(rd, rd);
-    float b = 2 * Vector3DotProduct(sphere_to_ray_origin, rd);
-    float c = Vector3DotProduct(sphere_to_ray_origin, sphere_to_ray_origin) - (sr * sr);
+    float b = 2 * Vector3DotProduct(CO, rd);
+    float c = Vector3DotProduct(CO, CO) - (sr * sr);
 
     float discriminant = (b * b) - (4 * a * c); 
 
     if (discriminant < 0)
     {
-      return (ray_sphere_intersection_info) {false, (Vector3){-1,-1,-1}};
+      return (ray_sphere_intersection_info) {false, (Vector2){-1,-1}};
     }
 
     float t1 = (-b + sqrt(discriminant)) / (2*a);
     float t2 = (-b - sqrt(discriminant)) / (2*a);
 
-    float tmin = fminf(t1, t2);
-
-    if (tmin > 0)
-    {
-      Vector3 p = Vector3Add(ro, Vector3Scale(rd, tmin));
-
-      return (ray_sphere_intersection_info) {true, p};
-    }
-
-    return (ray_sphere_intersection_info) {false, (Vector3){-1,-1,-1}};
+    return (ray_sphere_intersection_info) {true, (Vector2){t1,t2}};
   }
 // #endregion
+
+// #region Raytracing
+Color trace_ray(Sphere* spheres, int sphere_count, Vector3 ro, Vector3 rd, float tmin, float tmax)
+{
+  float closest_t = INFINITY;
+  bool haveFoundSphereToDraw = false;
+  Sphere closestSphere;
+
+  for (int i = 0; i < sphere_count; i++)
+  {
+    ray_sphere_intersection_info hit_info = ray_sphere_intersection(spheres[i].center, spheres[i].radius, ro, rd);
+
+    float t1 = hit_info.intersections.x;
+    float t2 = hit_info.intersections.y;
+
+    if (hit_info.is_intersecting)
+    { 
+      if (t1 > tmin && t1 < tmax)
+      {
+        if (t1 < closest_t)
+        {
+          haveFoundSphereToDraw = true;
+          closest_t = t1;
+          closestSphere = spheres[i];
+        }
+      }
+      
+      if (t2 > tmin && t2 < tmax)
+      {
+        if (t2 < closest_t)
+        {
+          haveFoundSphereToDraw = true;
+          closest_t = t2;
+          closestSphere = spheres[i];
+        }
+      }
+    }
+  }
+
+  if (haveFoundSphereToDraw)
+  {
+    return closestSphere.color;
+  }
+  else
+  {
+    return GOLD;
+  }
+}
+// #endregion
+
+#define sphere_count 3
 
 int main(void)
 {
@@ -80,40 +128,32 @@ int main(void)
 
     InitWindow(screenWidth, screenHeight, "Game");
     
-    Vector2 startPos = {20,20};
-    Vector2 endPos = {0,0};
-
+    Sphere spheres[sphere_count] = {
+      {.center = {0, -1, 3}, .radius = 1, RED},
+      {.center = {2, 0, 4}, .radius = 1, GREEN},
+      {.center = {-2, 0, 4}, .radius = 1, BLUE},
+    };
+    
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
-      endPos = GetMousePosition();
-
       BeginDrawing();
 
       ClearBackground(RAYWHITE);
 
-      Vector3 dir = Vector3Normalize(Vector3Subtract((Vector3){endPos.x, endPos.y, 0}, (Vector3){startPos.x, startPos.y, 0}));
-      dir = Vector3Subtract((Vector3){endPos.x, endPos.y, 0}, (Vector3){startPos.x, startPos.y, 0});
-
-      ray_sphere_intersection_info intersection_info = ray_sphere_intersection(
-      (Vector3){200,200,0},
-      50, 
-      (Vector3){startPos.x,startPos.y,0},
-      dir 
-      );
-
-      if (intersection_info.is_intersecting)
+      Vector3 ro = {0,0,0};
+      int w = GetScreenWidth();
+      int h = GetScreenWidth();
+      for (int x = -w/2; x < w/2; x++)
       {
-        DrawCircleV((Vector2){200,200}, 50, GREEN);
-        DrawCircleV((Vector2){intersection_info.hit_point.x, intersection_info.hit_point.y}, 10, BLACK);
-      }
-      else
-      {
-        DrawCircleV((Vector2){200,200}, 50, RED);
+        for (int y = -h/2; y < h/2; y++)
+        {
+          Vector3 rd = canvas_to_viewport(x, y);
+          Color color = trace_ray(spheres, sphere_count, ro, rd, 1, INFINITY);
+          canvas_put_pixel(x, y, color);
+        }
       }
 
-      DrawLineEx(startPos, endPos, 3, BLACK);
-      
       EndDrawing();
     }
     CloseWindow();
